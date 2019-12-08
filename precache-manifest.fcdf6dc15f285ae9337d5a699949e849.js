5812265bf745a224b40c68abd4272657b67d3bae
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-12-08 20:27:11 +0100

    Updates

---
+++ precache-manifest.fcdf6dc15f285ae9337d5a699949e849.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1fd7d57e7247eeac20b9648d778edf5",
+    "url": "/menuSPA/index.html"
+  },
+  {
+    "revision": "999845b71535f7ff60fa",
+    "url": "/menuSPA/static/css/main.77c00505.chunk.css"
+  },
+  {
+    "revision": "8900f424eb72424f141e",
+    "url": "/menuSPA/static/js/2.2c42103f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/menuSPA/static/js/2.2c42103f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "999845b71535f7ff60fa",
+    "url": "/menuSPA/static/js/main.bdaf985b.chunk.js"
+  },
+  {
+    "revision": "e37a4b6386b03e2f22f9",
+    "url": "/menuSPA/static/js/runtime-main.16152bbd.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/menuSPA/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/menuSPA/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/menuSPA/static/media/header3.d9d36b65.jpg"
+  }
+]);
