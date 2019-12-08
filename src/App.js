935e43a03b27bd04837a94c00c58e834c0e2bb42
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-12-08 17:00:35 +0100

    Add layouts and pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./layouts/Header";
+import Navigation from "./layouts/Navigation";
+import Page from "./layouts/Page";
+import Footer from "./layouts/Footer";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App">Menu</div>;
+    return (
+      <Router>
+        <div className="App">
+          <header>{<Header />}</header>
+          <main>
+            <aside>{<Navigation />}</aside>
+            <section className="page">{<Page />}</section>
+          </main>
+          <footer>{<Footer />}</footer>
+        </div>
+      </Router>
+    );
   }
 }
 
