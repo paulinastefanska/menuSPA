eb90ead2a375ebf27d724dfae3d71c410171a09d
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-12-08 18:49:56 +0100

    Add products page

--- src/layouts/Page.js
+++ src/layouts/Page.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import ProductPage from "../pages/ProductPage";
+import ProductListPage from "../pages/ProductListPage";
 import ContactPage from "../pages/ContactPage";
 import AdminPage from "../pages/AdminPage";
 import ErrorPage from "../pages/ErrorPage";
@@ -12,7 +13,8 @@ const Page = () => {
     <>
       <Switch>
         <Route path="/" exact component={HomePage} />
-        <Route path="/products" component={ProductPage} />
+        <Route path="/products" component={ProductListPage} />
+        <Route path="/product/:id" component={ProductPage} />
         <Route path="/contact" component={ContactPage} />
         <Route path="/admin" component={AdminPage} />
         <Route path="/login" component={LoginPage} />
--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Product from "../components/Product";
 
-const ProductsPage = () => {
-  return <div>Products</div>;
+const ProductsPage = ({ match }) => {
+  return (
+    <>
+      <h2>Product page</h2>
+      <Product id={match.params.id} />
+      <Link to="/products">Back</Link>
+    </>
+  );
 };
 
 export default ProductsPage;
