04f77b01aa96c000e71316cf708ded3302638460
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-12-08 17:53:25 +0100

    Add contact page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
+import { Prompt } from "react-router-dom";
+import "../styles/ContactPage.css";
 
-const ContactPage = () => {
-  return <div>Contact</div>;
-};
+class ContactPage extends Component {
+  state = {
+    value: ""
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      value: ""
+    });
+  };
+  handleChange = e => {
+    this.setState({
+      value: e.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div className="contact">
+        <form onSubmit={this.handleSubmit}>
+          <h3>Write to us</h3>
+          <textarea
+            value={this.state.value}
+            onChange={this.handleChange}
+            placeholder="write message..."
+          ></textarea>
+          <button>Send</button>
+        </form>
+        <Prompt
+          when={this.state.value}
+          message="You have not completed the form. Are you sure you want to leave?"
+        />
+      </div>
+    );
+  }
+}
 
 export default ContactPage;
